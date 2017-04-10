652150ab31c85f0dceeb0f2873b4a947844859f7
jamesjtong <jtong@yhmg.com>
2017-04-09 21:41:32 -0400

    minor change to game canvas cell borders

--- src/paintCell.js
+++ src/paintCell.js
@@ -1,8 +1,13 @@
 export default function paintCell(canvasContext, xCoordinate, yCoordinate, color, gameSettings) {
-  canvasContext.fillStyle = color;
-  canvasContext.fillRect(xCoordinate*gameSettings.cellWidth,
+  const rectArguments = [
+    xCoordinate*gameSettings.cellWidth,
     yCoordinate*gameSettings.cellWidth,
     gameSettings.cellWidth,
     gameSettings.cellWidth
-  );
+  ];
+  canvasContext.fillStyle = color;
+  canvasContext.fillRect(...rectArguments);
+
+  canvasContext.strokeStyle = 'red';
+  canvasContext.strokeRect(...rectArguments);
 }
